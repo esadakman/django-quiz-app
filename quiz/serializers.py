17175f2739bc2e2cd594915550afa785af87d6a2
esadakman <esadd26@gmail.com>
2022-09-26 22:51:44 +0300

    queistion, option serializers

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -19,6 +19,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class QuizSerializer(serializers.ModelSerializer):
     category = serializers.StringRelatedField()
+
     class Meta:
         model = Quiz
         fields = (
@@ -27,3 +28,33 @@ class QuizSerializer(serializers.ModelSerializer):
             'category',
             'question_count'
         )
+
+
+class OptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Option
+        fields = (
+            'id',
+            'option_text',
+            'is_right'
+        )
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    options = OptionSerializer(many=True)
+    quiz = serializers.StringRelatedField()
+
+    class Meta:
+        model = Question
+        fields = (
+            'id',
+            'quiz',
+            'title',
+            'options',
+            'difficulty'
+        )
+
+
+# title = models.TextField()
+#     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+#     difficulty = models.CharField(max_length=1, choices=SCALE)
--- quiz/urls.py
+++ quiz/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     CategoryList,
-    QuizList
+    QuizList,
+    QuestionList
 )
 
 urlpatterns = [
     path('', CategoryList.as_view()),
     path('quiz/', QuizList.as_view()),
+    path('question/', QuestionList.as_view()),
 ]
